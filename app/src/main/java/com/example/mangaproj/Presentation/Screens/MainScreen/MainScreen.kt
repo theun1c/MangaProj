b18a300d13c585f3579aaf99f959b435b560e52e
theun1c <belovdima1122@yandex.ru>
2025-04-29 01:21:04 +0300

    add search nar

--- app/src/main/java/com/example/mangaproj/Presentation/Screens/MainScreen/MainScreen.kt
+++ app/src/main/java/com/example/mangaproj/Presentation/Screens/MainScreen/MainScreen.kt
@@ -1,6 +1,7 @@
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.material3.*
@@ -28,6 +29,14 @@ fun MainScreen(navController: NavHostController) {
     val userState by authViewModel.userState
     val mangaList by mangaViewModel.mangaList.collectAsState()
 
+    // Состояние для поиска
+    var searchQuery by remember { mutableStateOf("") }
+
+    // Фильтруем список манги на основе поискового запроса
+    val filteredMangaList = mangaList.filter {
+        it.title.contains(searchQuery, ignoreCase = true)
+    }
+
     LaunchedEffect(Unit) {
         authViewModel.isUserLoggedIn(context)
         mangaViewModel.loadManga()
@@ -49,13 +58,32 @@ fun MainScreen(navController: NavHostController) {
                     .padding(paddingValues)
                     .padding(16.dp)
             ) {
+                // Поле для поиска
+                OutlinedTextField(
+                    value = searchQuery,
+                    onValueChange = { searchQuery = it },
+                    label = { Text("Поиск манги") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 16.dp)
+                        .height(56.dp), // Высота поля поиска для согласованности с кнопкой
+                    shape = RoundedCornerShape(15.dp), // Закругленные углы как у кнопки
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color(0xFF24b9bd), // Цвет границы при фокусе
+                        unfocusedBorderColor = Color(0xFF24b9bd), // Цвет границы в обычном состоянии
+                        focusedLabelColor = Color(0xFF24b9bd), // Цвет метки при фокусе
+                        unfocusedLabelColor = Color(0xFF24b9bd) // Цвет метки в обычном состоянии
+                    )
+                )
+
+                // Список манги
                 LazyColumn(
                     modifier = Modifier
                         .fillMaxWidth()
                         .weight(1f),
                     verticalArrangement = Arrangement.spacedBy(12.dp)
                 ) {
-                    items(mangaList) { manga ->
+                    items(filteredMangaList) { manga ->
                         MangaCardStyled(manga = manga)
                     }
                 }
